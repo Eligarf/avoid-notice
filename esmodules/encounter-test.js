0ae99032c26c6d1fd105f6a3c7facb5522d17ac5
Eligarf <eligarf@live.com>
2026-07-06 12:48:56 -0700

    NPC rolls happening

--- esmodules/encounter-test.js
+++ esmodules/encounter-test.js
@@ -2,59 +2,96 @@ import { isAvoider } from "./menu.js";
 import { MODULE_ID } from "./const.js";
 import { localizeString, debuglog, iterateTokensAndParties } from "./main.js";
 
-export async function encounterTest(tokens) {
-  const avoiders = [];
-  await iterateTokensAndParties(tokens, async (token) => {
-    if (isAvoider(token)) {
-      if (!avoiders.includes(token)) avoiders.push(token);
-    }
+async function rollStealth(token) {
+  const actor = token.actor;
+  const skill = actor?.skills?.stealth;
+  const roll = skill.roll({
+    rollMode: "selfroll",
+    skipDialog: true,
+    createMessage: false,
+    traits: ["secret", "exploration"],
   });
+  debuglog("rollStealth", { token, actor, skill, roll });
+  return roll;
+}
 
-  const friendlyAvoiders = avoiders.filter(
-    (token) => token.document.disposition === 1,
-  );
-  const enemies = tokens.filter((token) => token.document.disposition === -1);
-
-  let content = `<div><h3>${localizeString(`${MODULE_ID}.encounter.name`)}</h3></div>`;
-  let actions = { party: {} };
-  for (const token of friendlyAvoiders) {
-    const actionId = foundry.utils.randomID();
-    actions.party[actionId] = { tokenId: token.id };
-    content += `<button class="avoid-notice-pc" data-action-id="${actionId}" data-module="${MODULE_ID}">${token.name}</button></br>`;
-  }
-
-  const enemyAvoiders = avoiders.filter(
-    (token) => token.document.disposition === -1,
-  );
+export async function encounterTest(tokens) {
+  // Get our list of friendly and enemy tokens
   let friendlies = [];
   await iterateTokensAndParties(tokens, async (token) => {
     if (token.document.disposition === 1) {
       if (!friendlies.includes(token)) friendlies.push(token);
     }
   });
+  const enemies = tokens.filter((token) => token.document.disposition === -1);
 
-  content += `<div data-visibility="gm">`;
+  // Find the friendly avoiders and enemies
+  const friendlyAvoiders = friendlies.filter((token) => isAvoider(token));
+  const enemyAvoiders = enemies.filter((token) => isAvoider(token));
+  let friendlyStealth = {};
+  let enemyStealth = {};
+
+  // Build interaction buttons for friendly avoiders
+  let actions = { friendly: {} };
+  let content = `<div class="${MODULE_ID}-encounter-test">${localizeString(`${MODULE_ID}.encounter.title`)}</div>`;
+  content += `<div class="friendlies">`;
+  for (const token of friendlyAvoiders) {
+    const actionId = foundry.utils.randomID();
+    actions.friendly[actionId] = { tokenId: token.id };
+    const roll = await rollStealth(token);
+    friendlyStealth[token.id] = roll;
+    content += `
+      <div class="friendly">
+        <span class="name">${token.name}</span>
+        <span class="roll">${roll.total}</span>
+      </div>`;
+    // <div class=${MODULE_ID}-pc>
+    //   <span class="name">${token.name}</span>
+    //   <button class="${MODULE_ID}-button" data-action-id="${actionId}" data-module="${MODULE_ID}">${token.name}</button>
+  }
+  content += `</div>`;
+
+  // Find the enemy avoiders and friendly observers
+
+  content += `<div class="enemies" data-visibility="gm">`;
   const actionId = foundry.utils.randomID();
   actions[actionId] = [];
   for (const token of enemyAvoiders) {
-    actions[actionId].push(token.id);
-    content += `${token.name}</br>`;
+    const roll = await rollStealth(token);
+    enemyStealth[token.id] = roll.total;
+    content += `
+      <div class="enemy">
+        <span class="name">${token.name}</span>
+        <span class="roll">${roll.total}</span>
+      </div>`;
   }
-  content += `<button class="avoid-notice-npcs" data-action-id="${actionId}" data-module="${MODULE_ID}">${localizeString(`${MODULE_ID}.encounter.npcs`)}</button>`;
+  // content += `
+  //   <div class="${MODULE_ID}-npc-roll">
+  //     <button class="avoid-notice-npcs" data-action-id="${actionId}" data-module="${MODULE_ID}">
+  //       ${localizeString(`${MODULE_ID}.encounter.npcs`)}
+  //     </button>
+  //   </div>`;
 
-  const gmIds = game.users.filter((u) => u.isGM).map((u) => u.id);
   await ChatMessage.create({
     content,
-    whisper: gmIds,
     rollmode: "gmroll",
     flags: {
       [MODULE_ID]: {
-        actions,
-        enemyIds: enemyAvoiders.map((token) => token.id),
-        partyIds: friendlies.map((token) => token.id),
+        encounterTest: {
+          actions,
+          enemyIds: enemies.map((token) => token.id),
+          partyIds: friendlies.map((token) => token.id),
+        },
       },
     },
   });
 }
 
-Hooks.on("renderChatMessage", (message, html, data) => {});
+Hooks.on("renderChatMessageHTML", (message, html, data) => {
+  const encounterTest = message.flags[MODULE_ID]?.encounterTest;
+  if (!encounterTest) return;
+  debuglog("renderChatMessageHTML", { message, html, data, encounterTest });
+  // const selected = html.querySelectorAll(
+  //   `.${MODULE_ID}-button, .avoid-notice-npcs`,
+  // );
+});
