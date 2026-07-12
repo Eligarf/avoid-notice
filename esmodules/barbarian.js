58534ce1e445196198c08eed975cf40c745b8987
Eligarf <eligarf@live.com>
2026-07-12 16:57:38 -0700

    Fix hacky method for controlling token (#49)

--- esmodules/barbarian.js
+++ esmodules/barbarian.js
@@ -1,6 +1,7 @@
 import { log } from "./main.js";
 
 export async function enrageBarbarians(pcs) {
+  const beforeV14 = Number(game.version.split()[0]) < 14;
   const quickTempered = pcs.filter((c) =>
     c.actor.items.some((i) => i?.system?.slug === "quick-tempered"),
   );
@@ -9,9 +10,17 @@ export async function enrageBarbarians(pcs) {
       (i) => i?.system?.slug === "rage" && i?.system?.selfEffect,
     );
     if (!rage) continue;
-    const object = barbarian.token?._object;
-    if (!object?.control) continue;
-    object.control();
+    if (beforeV14) {
+      const object = barbarian.token?._object;
+      if (!object?.control) continue;
+      object.control();
+    } else {
+      const token = canvas.tokens.placeables.find(
+        (t) => t.id === barbarian.token.id,
+      );
+      if (!token) continue;
+      token.control({ releaseOthers: true });
+    }
     await game.pf2e.rollItemMacro(rage.id);
   }
 }
