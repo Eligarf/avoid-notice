cad780e60331b4b80e0462e102435bc5ad414c48
Eligarf <eligarf@live.com>
2026-07-31 10:41:40 -0700

    hooked up undo events

--- esmodules/tokens-menu.js
+++ esmodules/tokens-menu.js
@@ -1,6 +1,6 @@
 import { AvoidNoticePopupMenu } from "./menu.js";
 import { setAsAmbushers, clearActorStealth } from "./stealth.js";
-import { MODULE_ID } from "./const.js";
+import { MODULE_ID, SLUGS } from "./const.js";
 import { getVisibilityHandler } from "./main.js";
 import {
   localizeString,
@@ -17,16 +17,85 @@ export async function invokeTokensMenu({ selection }) {
 
   let choices = [];
   const visibilityHandler = getVisibilityHandler();
-  if (visibilityHandler !== "visioner")
-    choices.push({
-      key: "remove-stealth",
-      label:
-        selection.type === "controlled"
-          ? game.i18n.localize(
-              `${MODULE_ID}.menu.removeControlledStealth.label`,
-            )
-          : game.i18n.localize(`${MODULE_ID}.menu.removeTargetedStealth.label`),
-    });
+  if (visibilityHandler !== "visioner") {
+    const stealthers = selection.tokens.filter((token) =>
+      token.actor?.items?.some((i) => i.slug === SLUGS.stealthEffect),
+    );
+
+    if (stealthers.length > 0) {
+      choices.push({
+        key: "remove-stealth",
+        label:
+          selection.type === "controlled"
+            ? game.i18n.localize(
+                `${MODULE_ID}.menu.removeControlledStealth.label`,
+              )
+            : game.i18n.localize(
+                `${MODULE_ID}.menu.removeTargetedStealth.label`,
+              ),
+        hint:
+          selection.type === "controlled"
+            ? game.i18n.localize(
+                `${MODULE_ID}.menu.removeControlledStealth.hint`,
+              )
+            : game.i18n.localize(
+                `${MODULE_ID}.menu.removeTargetedStealth.hint`,
+              ),
+      });
+
+      const hiddenObserved = stealthers.some((token) => {
+        const stealth = token.actor?.items?.find(
+          (i) => i.slug === SLUGS.stealthEffect,
+        );
+        const exceptions = stealth?.flags?.[MODULE_ID]?.hidden;
+        return exceptions?.exceptFor?.length > 0;
+      });
+      debuglog("hiddenObserved", hiddenObserved);
+      const undetectedObserved = stealthers.some((token) => {
+        const stealth = token.actor?.items?.find(
+          (i) => i.slug === SLUGS.stealthEffect,
+        );
+        const exceptions = stealth?.flags?.[MODULE_ID]?.undetected;
+        return exceptions?.exceptFor?.length > 0;
+      });
+      debuglog("undetectedObserved", undetectedObserved);
+
+      if (hiddenObserved && undetectedObserved) {
+        choices.push({
+          key: "undo-hidden",
+          label: game.i18n.localize(
+            `${MODULE_ID}.menu.undoHiddenReveals.label`,
+          ),
+          hint: localizeString(`${MODULE_ID}.menu.undoHiddenReveals.hint`, {
+            type: game.i18n.localize(
+              `${MODULE_ID}.menu.type.${selection.type}`,
+            ),
+          }),
+        });
+        choices.push({
+          key: "undo-undetected",
+          label: game.i18n.localize(
+            `${MODULE_ID}.menu.undoUndetectedReveals.label`,
+          ),
+          hint: localizeString(`${MODULE_ID}.menu.undoUndetectedReveals.hint`, {
+            type: game.i18n.localize(
+              `${MODULE_ID}.menu.type.${selection.type}`,
+            ),
+          }),
+        });
+      } else if (hiddenObserved || undetectedObserved) {
+        choices.push({
+          key: "undo-reveals",
+          label: game.i18n.localize(`${MODULE_ID}.menu.undoReveals.label`),
+          hint: localizeString(`${MODULE_ID}.menu.undoReveals.hint`, {
+            type: game.i18n.localize(
+              `${MODULE_ID}.menu.type.${selection.type}`,
+            ),
+          }),
+        });
+      }
+    }
+  }
 
   const combat = game?.combat;
   if (!combat && !selection.dispositions.has(1)) {
@@ -49,6 +118,10 @@ export async function invokeTokensMenu({ selection }) {
     });
   }
 
+  if (choices.length === 0) {
+    ui.notifications.info(game.i18n.localize(`${MODULE_ID}.menu.nothingToDo`));
+    return;
+  }
   choices.sort((a, b) => a.label.localeCompare(b.label));
   const choice = await AvoidNoticePopupMenu.show(title, choices);
 
@@ -69,6 +142,15 @@ export async function invokeTokensMenu({ selection }) {
     case "test-avoidance":
       debuglog("test-avoidance", selection.tokens);
       await testAvoidance(selection.tokens, choice.secret);
+      break;
+    case "undo-hidden":
+      debuglog("undo-hidden", selection.tokens);
+      break;
+    case "undo-undetected":
+      debuglog("undo-undetected", selection.tokens);
+      break;
+    case "undo-reveals":
+      debuglog("undo-reveals", selection.tokens);
       break;
   }
 }
